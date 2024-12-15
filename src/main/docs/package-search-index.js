d702cb69a184e5c46ea2b002074025400c42dcf2
Viktoriia <beloborodova.viktoriia@gmail.com>
2024-12-15 16:18:50 +0200

    Update JavaDoc

--- src/main/docs/package-search-index.js
+++ src/main/docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"eduni.distributions"},{"l":"framework"},{"l":"model"},{"l":"view"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"dao"},{"l":"datasource"},{"l":"eduni.distributions"},{"l":"entity"},{"l":"framework"},{"l":"model"},{"l":"view"}];updateSearchResults();
